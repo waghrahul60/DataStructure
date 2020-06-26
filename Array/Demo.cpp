e1e11de59605ced303f9e16afbc2a551fa2b9559
waghrahul60 <waghrahul60@gmail.com>
2020-06-26 09:28:28 +0530

    Struct full

---
+++ Array/Demo.cpp
@@ -0,0 +1,33 @@
+//Basic program to access data from the node
+
+#include<iostream>
+
+using namespace std;
+
+//Creating Node Structure
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+//Creating Head Pointer And equating to NULL
+
+Node *head = NULL;
+
+int main()
+{
+    //Creating a new node
+    Node * ptr = new Node();
+
+    //Adding data and setting next = NULL
+    ptr->data = 20;
+    ptr->next = NULL;
+
+    //Pointing Head to create node
+    head=ptr;
+    cout<<"data is :"<<head->data;
+    return 0;
+
+}
