b6afb5614a83d1142691919b35c47360742a181d
waghrahul60 <waghrahul60@gmail.com>
2020-12-13 22:47:38 +0530

    struct

---
+++ Linked List/Singly/Struct/SinglelinkedList.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+
+using namespace std;
+
+struct Node
+{
+   int data;
+   struct Node* next;
+};
+
+struct Node* head;
+
+void insertNode(int data)
+{   
+    if (head == NULL)
+    {
+        struct Node * New_node = (struct Node*)malloc(sizeof(struct Node));
+        New_node->data = data;
+        New_node->next = NULL;
+        head= New_node;
+    }
+    else
+    {
+        struct Node * New_node = (struct Node*)malloc(sizeof(struct Node));
+        New_node->data = data;
+        New_node->next = head;
+        head= New_node;
+    }
+    
+}
+
+void Display()
+{
+    struct Node * temp = head;
+    while (temp != NULL)
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    insertNode(10);
+    insertNode(20);
+    insertNode(30);
+    cout<<"Rahul";
+
+    Display();
+    return 0;
+}
